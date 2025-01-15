1f63c3bc91949db28787babdc3aa76fd81ea628b
Mantillnow <manuelshaji206@gmail.com>
2025-01-15 15:59:07 +0530

    Update strcopy.c

--- strcopy.c
+++ strcopy.c
@@ -5,14 +5,21 @@ int main()
     char str[20],str2[20];
     printf("enter a string:");
     scanf("%s",str);
+    printf("enter string 2:");
+    scanf("%s",str2);
     
     int i=0;
     while(str[i]!='\0')
     {
-        str2[i]=str[i];
         i++;
     }
-    str2[i]='\0';
-    printf("%s",str2);
+    int j=0;
+    while(str2[j]!='\0')
+    {
+        str[i]=str2[j];
+        j++;
+        i++;
+    }
+    printf("%s",str);
     return 0;
 }
